4c51ccf3a72a809980a30b6970730ac41877108f
Maureen O'Flynn <maureen.p.oflynn@gmail.com>
2022-02-16 21:58:12 -0800

    transactionFilters file created

---
+++ src/transactions/transactionFilters.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CategoryFilter from './CategoryFilter';
+
+function TransactionFilters() {
+    return 
+        <p>Filter By Payee </p>
+}
+
+
+// what natalie demonstrated
+export default function TransactionFilters() {
+  return (
+    <div>
+      <CategoryFilter />
+    </div>
+  );
+}
