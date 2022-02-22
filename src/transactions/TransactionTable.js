78ed96a04e2d154fde10e50ba632d4b7efd44519
Maureen O'Flynn <maureen.p.oflynn@gmail.com>
2022-02-21 21:46:50 -0800

    BUD-10 attempts at filtering By category

--- src/transactions/TransactionTable.js
+++ src/transactions/TransactionTable.js
@@ -4,7 +4,11 @@ import { v4 as uuid } from 'uuid';
 import { useSelector } from 'react-redux';
 import TransactionRow from './TransactionRow';
 import TransactionForm from './TransactionForm';
+import SortByData from './SortArrayObjects';
+// import CategoryDetailsPage from '../budget/CategoryDetailsPage';
+// import { categoryByIdSelector } from '../budget/CategorySlice';
 
+// import CategorySlice, { categoryByIdSelector } from '../budget/CategorySlice';
 /**
  * Shows a table of transactions and optionally a form for creating a transaction
  *
@@ -15,6 +19,17 @@ import TransactionForm from './TransactionForm';
 function TransactionTable({ isCreating = false, onStopCreating }) {
   // Get all the transactions to show in the table
   const transactionList = useSelector((state) => state.transactions.entities);
+  // const sortById = categoryByIdSelector((state) => state.transactions.entities);
+  // const sortById = ((state) => state.transactions.entities);
+  // const sortById = useSelector((state) => state.transactionReducer.entities);
+  // const sortById = useSelector((state) => state.transactions.entities.categoryId);
+
+  // attempted to make a simple array to console log; nope
+  // const array2 = ['0', '91', '32', '43', '84', '95'];
+  // function sortById() {
+  //   sortById.sort(array2);
+  //   console.log(sortById);
+  // }
 
   // JSX
   return (
@@ -23,9 +38,27 @@ function TransactionTable({ isCreating = false, onStopCreating }) {
         <tr>
           <th>Date</th>
           <th>Payee</th>
-          <th>Category</th>
-          <th>Notes</th>
-          <th>Amount</th>
+          <div>
+            {/* added selectBox to attempt to sort category  */}
+            {/* then tried about 100 different ways to sort */}
+            <select id="selectBox" onChange={() => SortByData()}>
+              {/* must be easier way.instead of Watching tutorials */}
+              {/* i banged by head against a wallhopin i could get this */}
+              <option>Select</option>
+              <option value="0">Income</option>
+              <option value="1">Rent</option>
+              <option value="2">Groceries</option>
+              <option value="3">Gas</option>
+              <option value="4">Shopping</option>
+              <option value="5">Eating Out</option>
+            </select>
+          </div>
+          <th searchBox="">Notes</th>
+          <th sortBy="">Amount</th>
+          <div>
+            {/* attempted to console log to see if any sort filters i made were working; nope */}
+            <button type="button" onClick={() => SortByData()}>console log</button>
+          </div>
           <th aria-label="Actions" />
         </tr>
       </thead>
