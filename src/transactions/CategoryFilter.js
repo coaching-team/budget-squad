30826a4da9c9aa66c6f9a4c02195e9587293fee1
Stephanie Alarcia <scalarcia@gmail.com>
2022-03-21 11:19:44 -0700

    Added functional CategoryFilter and Transaction Filter and tied these into TransactionPage of index

--- src/transactions/CategoryFilter.js
+++ src/transactions/CategoryFilter.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import { CATEGORIES } from '../data';
 
-function CategoryFilter(props) {
+function CategoryFilter({ handleFilter }) {
+  function onSelectChanged(event) {
+    const { value } = event.target;
+
+    handleFilter({ categoryId: value });
+  }
+
+  function onClear() {
+    handleFilter({ categoryId: null });
+  }
 
   return (
-    <div>
-      <select name="categoryFilter" onChange={props.handleFilter}>
-        <option>Filter by Category</option>
-        {CATEGORIES.map((category) => (
-          <option key={category.id} value={category.id} name={category.name}>
-            {category.name}
-          </option>
-        ))}
-      </select>
-      <button onClick={props.clearFilter}>Clear Filter</button>
+    <div className="row">
+      <div className="col col-3 mr-2">
+        <select className="form-select" name="categoryFilter" onChange={onSelectChanged}>
+          <option>Filter by Category</option>
+          {CATEGORIES.map((category) => (
+            <option key={category.id} value={category.id} name={category.name}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="col col-3">
+        <button className="btn btn-outline-secondary" type="submit" onClick={onClear}>Clear Filter</button>
+      </div>
     </div>
   );
 }
+
+CategoryFilter.propTypes = {
+  handleFilter: PropTypes.func.isRequired,
+};
 
 export default CategoryFilter;
--- src/transactions/index.js
+++ src/transactions/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import TransactionTable from './TransactionTable';
 import TransactionFilters from './TransactionFilters';
 
-
 /**
  * Shows the list of transactions and allows the user to create transactions
  *
@@ -13,9 +12,8 @@ import TransactionFilters from './TransactionFilters';
 export default function TransactionPage() {
   // Local State - is a transaction currently being created, for passing to the TransactionTable
   const [isCreating, setisCreating] = useState(false);
-  const [filter, setFilter] = useState("No filters applied");
-  //need another useState hook to update state of filtering in response to receiving input from Transaction Filter
-  //need function to handle sending these changes to TransactionTable 
+  const [filter, setFilter] = useState({ categoryId: null });
+
   const handleStopCreating = () => {
     setisCreating(false);
   };
@@ -26,17 +24,23 @@ export default function TransactionPage() {
       <div className="row mb-5 mt-4">
         <div className="col">
           <h3 className="mt-1">Transactions</h3>
-          <p>Category Id from transaction filters: {filter}</p>
-          {/* not appearing */}
+          <p>
+            Category Id from transaction filters:
+            {filter.categoryId}
+          </p>
+
         </div>
         <div className="col text-end">
           <button className="btn btn-info" type="button" onClick={() => setisCreating(true)}>New Transaction</button>
         </div>
       </div>
-      <div className="row mt-3">
+      <div className="row">
         <div className="col">
           <TransactionFilters filter={filter} setFilter={setFilter} />
-          {/* not appearing */}
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
           <TransactionTable isCreating={isCreating} onStopCreating={handleStopCreating} />
         </div>
       </div>
