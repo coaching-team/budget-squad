345de634cf47e7a2ee3e1a528395a10334c9a452
Vanessa Alarcia <vcalarcia@gmail.com>
2022-05-16 18:36:34 -0700

    added form validation to 'create category' form

--- src/budget/NewCategoryPage.js
+++ src/budget/NewCategoryPage.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 import { addNewCategory } from './CategorySlice';
+import {
+  validate, required, alphanumericCharactersOnly, limitedCharacters, restrictedRange, notANumber,
+} from './FormValidation';
 
 /**
  * Shows a form that allows the user to create a new budgeting category
@@ -17,20 +21,34 @@ export default function NewCategoryPage() {
 
   // Local State: in-progress form data
   const [name, setName] = useState('');
-  const [target, setTarget] = useState('');
+  const [amount, setAmount] = useState('');
 
-  const handleCreateCategory = () => {
-    const newCategory = {
-      name,
-      target,
-    };
-    dispatch(addNewCategory(newCategory));
-    navigate('/budget');
+  const [errors, setErrors] = useState({ name: [], amount: [] });
+
+  const handleAmountChange = (event) => {
+    const amountInput = event.target.value;
+    setAmount(amountInput * 1);
+    setErrors((currentErrors) => ({ ...currentErrors, amount: validate(amountInput, 'Amount', [required, restrictedRange, notANumber]) }));
   };
 
-  const handleTargetChange = (event) => {
-    const targetInput = parseInt(event.target.value, 10);
-    setTarget(targetInput);
+  const handleNameChange = (event) => {
+    const nameInput = event.target.value;
+    setName(nameInput);
+    setErrors((currentErrors) => ({ ...currentErrors, name: validate(nameInput, 'Name', [required, alphanumericCharactersOnly, limitedCharacters]) }));
+  };
+
+  // Creates a new category on form submission
+  const handleCreateCategory = () => {
+    validate(name, 'Name', [required, alphanumericCharactersOnly, limitedCharacters]);
+    validate(amount, 'Amount', [required, restrictedRange, notANumber]);
+    if ((errors.amount.length === 0) && (errors.name.length === 0)) {
+      const newCategory = {
+        name,
+        amount,
+      };
+      dispatch(addNewCategory(newCategory));
+      navigate('/budget');
+    }
   };
 
   return (
@@ -45,13 +63,15 @@ export default function NewCategoryPage() {
           <form>
             <div className="mb-3">
               <label htmlFor="formName" className="form-label">Name</label>
-              <input type="text" onChange={(event) => setName(event.target.value)} className="form-control" name="formName" id="formName" />
+              <input type="text" onChange={handleNameChange} className={(errors.name.length !== 0) ? 'form-control invalid' : 'form-control'} name="formName" id="formName" required />
+              {errors && <div className="errors">{errors.name.map((message) => <div className="pt-1" key={uuid()}>{message}</div>)}</div>}
             </div>
             <div className="mb-3">
-              <label htmlFor="formTarget" className="form-label">Target</label>
-              <input type="text" onChange={handleTargetChange} className="form-control" name="formTarget" id="formTarget" />
+              <label htmlFor="formTarget" className="form-label">Amount</label>
+              <input type="text" onChange={handleAmountChange} className={(errors.amount.length !== 0) ? 'form-control invalid' : 'form-control'} name="formTarget" id="formTarget" required />
+              {errors && <div className="errors">{errors.amount.map((message) => <div className="pt-1" key={uuid()}>{message}</div>)}</div>}
             </div>
-            <button type="button" className="btn btn-primary mt-4" onClick={handleCreateCategory}>Create</button>
+            <button type="button" disabled={(errors.name.length !== 0) || (errors.amount.length !== 0) || (!name) || (!amount)} className="btn btn-primary mt-4" onClick={handleCreateCategory}>Create</button>
           </form>
         </div>
       </div>
